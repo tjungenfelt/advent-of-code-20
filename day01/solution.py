5a900d8db6db350e5419fe4366533934e590b66e
Tove Jungenfelt <tove.jungenfelt@gmail.com>
2020-12-04 22:03:25 +0100

    add part 2 sol1

--- day01/solution.py
+++ day01/solution.py
@@ -1,11 +1,21 @@
 import fileinput
+import itertools
 
 number_list = []
 with fileinput.input(files=('input.txt')) as f:
     for line in f:
         number_list.append(int(line))
+
+# PART 1
 seen = set()
 for number in number_list:
     if 2020-number in seen:
         print((2020-number)*number)
     seen.add(number)
+
+# PART 2
+comb_list = list(itertools.combinations(number_list, 3))
+for combination in comb_list:
+    x, y, z = combination
+    if x+y+z == 2020:
+        print(x*y*z)
